fa82c45397d0a990446aebc2b0cc7bfcc40d9b9d
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-03-03 07:49:30 -0500

    PROC-40: nuevas tecnologias, se explica ciclo for

---
+++ cicloFor.py
@@ -0,0 +1,18 @@
+# #arranca en 1, va hasta 20, aumenta de 1 en 1
+# for i in range (20):
+#     print(i)
+
+# #se puede presentar de esta forma, arranca en 1, va hasta 20 y los aumentos son de 5 en 5
+# for i in range (1,20,5):
+#     print(i)
+
+# #un uso principal de los for es para ARREGLOS, que en PYTHON se llaman LISTAS, compuestas de elementos
+# nombres = ["juan","carlos","martha"]
+# print(nombres)
+# print(nombres[1])
+# print(nombres[2])
+
+#for para RECORRER LISTAS en PY
+nombres = ["juan","carlos","martha"]
+for nombres in nombres:
+    print(nombres)
